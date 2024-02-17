9e6c40b68b603d4e78517783eb4177a52d9c71d2
José Luis Gutierrez <joselugz03@gmail.com>
2024-02-17 16:38:11 -0300

    add async router dependecy

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,16 @@
-import { Router, Route } from "preact-router"
+import { Router, Route, } from "preact-router"
 import { Home } from "./page/Home";
-import { Codebar } from "./page/Codebar";
+import AsyncRoute from "preact-async-route";
 
 export function App() {
   return (
     <Router>
       <Route component={Home} path="/" />
-      <Route component={Codebar} path="/:codebar" />
+      <AsyncRoute
+        path="/:codebar"
+        getComponent={() => import('./page/Codebar').then(module => module.Codebar)}
+        loading={() => <h1 class="text-gray-800 dark:text-white">Loading...</h1>}
+      />
     </Router>
   );
 }
